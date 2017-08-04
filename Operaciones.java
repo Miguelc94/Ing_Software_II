fc77dab91d723ff32931bc756eecfce01017b58a
Miguelc94 <30513413+Miguelc94@users.noreply.github.com>
2017-08-03 20:28:52 -0500

    Update Operaciones.java

--- Operaciones.java
+++ Operaciones.java
@@ -2,19 +2,19 @@ package software2.calculadora;
 
 public class Operaciones {
 	
-	public double sumar(double a, double b){
-		return a + b;
+	public double sumar(double n1, double n2){
+		return n1 + n2;
 	}
 	
-	public double restar(double a, double b){
-		return a - b;
+	public double restar(double n1, double n2){
+		return n1 - n2;
 	}
 	
-	public double multiplicar(double a, double b){
-		return a * b;
+	public double multiplicar(double n1, double n2){
+		return n1 * n2;
 	}
 	
-	public double dividir(double a, double b){
-		return a / b;
+	public double dividir(double n1, double n2){
+		return n1 / n2;
 	}
 }
